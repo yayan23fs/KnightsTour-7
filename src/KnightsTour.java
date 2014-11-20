99d1a038cc227420dcb8aa24e4fc70dd1b828a49
Laksha-Prashanth <lakshaprashanth@gmail.com>
2014-11-21 03:16:30 +0530

    Still continuing. Coding the possible places.

--- src/KnightsTour.java
+++ src/KnightsTour.java
@@ -1,5 +1,5 @@
 import javax.swing.JOptionPane;
-
+import java.util.*;
 
 public class KnightsTour {
 	
@@ -42,12 +42,79 @@ public class KnightsTour {
 		return true;
 	}
 	
-	public static int[][] validPlaces(int r,int c)
+	public static LinkedList validPlaces(int r,int c)
 	{
-		int[][] a;
+		LinkedList list = new LinkedList();
+		
+		int a[] = new int[2];
 		
 		
-		return a;
+		if(canMove(r,c,r-2,c+1)==true) 		
+		{
+			a[0]=r-2;
+			a[1]=c+1;
+			list.add(a);
+			System.out.println(r-2+","+c+1);
+		}
+		
+		if(canMove(r,c,r-1,c+2)==true) 			//
+		{
+			a[0]=r-1;
+			a[1]=c+2;
+			list.add(a);
+			System.out.println(r-1+","+c+2);
+		}
+		
+		if(canMove(r,c,r+1,c+2)==true) 			//
+		{
+			a[0]=r+2;
+			a[1]=c+2;
+			list.add(a);
+			System.out.println(r+1+","+c+2);
+		}
+		
+		if(canMove(r,c,r+2,c+1)==true) 			//
+		{
+			a[0]=r+2;
+			a[1]=c+1;
+			list.add(a);
+			System.out.println(r+2+","+c+1);
+		}
+		
+		if(canMove(r,c,r+2,c-1)==true) 			//
+		{
+			a[0]=r+2;
+			a[1]=c-1;
+			list.add(a);
+			System.out.println(r+2+","+(c-1));
+		}
+		
+		if(canMove(r,c,r-1,c-2)==true) 			//
+		{
+			a[0]=r-1;
+			a[1]=c-2;
+			list.add(a);
+			System.out.println(r+1+","+(c-2));
+		}
+		
+		if(canMove(r,c,r-1,c-2)==true) 			//
+		{
+			a[0]=r-1;
+			a[1]=c-2;
+			list.add(a);
+			System.out.println(r-1+","+(c-2));
+		}
+		
+		if(canMove(r,c,r-2,c-1)==true)
+		{
+			a[0]=r-2;
+			a[1]=c-1;
+			list.add(a);
+			System.out.println(r-2+","+(c-1));
+		}
+		
+		
+		return list;
 	}
 	
 	public static boolean knight(int r,int c)
@@ -69,9 +136,14 @@ public class KnightsTour {
 		
 		display();
 		
-		System.out.println(canMove(0,0,0,0));
+		System.out.println(canMove(0,0,1,2));
 		
+		LinkedList b = validPlaces(0,0);
 		
+		for(int i=0;i<b.size();i++)
+		{
+			System.out.println(((int [])b.get(i))[0]+","+((int [])b.get(i))[1]);
+		}
 		
 	}
 }
