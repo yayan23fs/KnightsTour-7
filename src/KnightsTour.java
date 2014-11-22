80cdfbb7950bf0bc01cad55cf9ccf6fe271459a8
Laksha-Prashanth <lakshaprashanth@gmail.com>
2014-11-22 20:39:07 +0530

    Doing the GUI.
    1 change in the knight function.

--- src/KnightsTour.java
+++ src/KnightsTour.java
@@ -1,9 +1,18 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 
 public class KnightsTour {
 	
 	public static int[][] array;
+	
+	public static JFrame frame;
+	
+	public static int x,y;
+	
+	public static boolean isPlaced;
 	
 	public static int solutions;
 	
@@ -47,7 +56,7 @@ public class KnightsTour {
 	
 	public static int[][] validPlaces(int r,int c)
 	{
-		List<String> list = new ArrayList<String>();
+		java.util.List<String> list = new ArrayList<String>();
 		
 		String s = "";
 		
@@ -131,29 +140,32 @@ public class KnightsTour {
 	public static void knight(int r,int c)
 	{
 		int[][] a = validPlaces(r,c);
-		
+		array[r][c]=1;
 		for(int i=0;i<a.length;i++)
 		{
 			array[a[i][0]][a[i][1]] = 1;
 			//display();
 			if (checkOnes() == true)
 			{
+				//Got solution
 				solutions++;
+				//System.out.println("solved");
+				
 			}
 			knight(a[i][0], a[i][1]);
 		}
 		array[r][c]=0;
 	}
 	
-	public static void main(String args[])
+	public static void initialise()
 	{
-		solutions=0;
+		x=y=0;
+		isPlaced = false;
 		String s=JOptionPane.showInputDialog("Enter the size of the board");
-		
 		int n = Integer.parseInt(s.charAt(0)+"");
 		int m = Integer.parseInt(s.charAt(2)+"");
-		
 		array = new int[n][m];
+		
 		for(int i=0;i<array.length;i++)
 		{
 			for(int j=0;j<array[0].length;j++)
@@ -161,10 +173,135 @@ public class KnightsTour {
 				array[i][j]=0;
 			}
 		}
+	}
+	
+	public static void drawChessBoard(Graphics g)
+	{
+		int FWidth = frame.getWidth();
+		int FHeight = frame.getHeight()-22;
+		
+		for(int i=0;i<array.length;i++)
+		{
+			for(int j=0;j<array[0].length;j++)
+			{
+				if((i+j)%2==0)
+					g.setColor(Color.white);
+				else
+					g.setColor(Color.black);
+				
+				int width = FWidth/array[0].length;
+				int height = (FHeight*2/3)/array.length;
+				
+				g.fillRect(j*width, i*height, width, height);
+			}
+			
+		}
+	}
+	
+	public static void placeKnight(Graphics g)
+	{
+		int r = getGridPosition()[0];
+		int c = getGridPosition()[1];
+		System.out.println("r: "+r+" c: "+c);
+		int FWidth = frame.getWidth();
+		int FHeight = frame.getHeight();
+		
+		int width = Math.floorDiv(FWidth,array[0].length);
+		int height = Math.floorDiv((FHeight*2/3),array.length);
+		
+		int x = r*height+height/3;
+		int y = c*width+width/3;
 		
 		
-		System.out.println();
-		System.out.println();
 		
+		g.fillOval(y, x, 25, 25);
+		
+	}
+	
+	public static class Panel extends JPanel
+	{
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g);
+			
+			int FWidth = frame.getWidth();
+			int FHeight = frame.getHeight()-22;
+			
+			
+			drawChessBoard(g);
+			
+			g.setColor(Color.BLUE);
+			if(isPlaced==true)
+				placeKnight(g);
+			
+		}
+		
+	}
+	
+	
+	public static class Mouse implements MouseListener
+	{
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			x=e.getX();
+			y=e.getY();
+			System.out.println(x+","+y);
+			isPlaced = true;
+			frame.paintComponents(frame.getGraphics());
+			getGridPosition();
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public static int[] getGridPosition()
+	{
+		
+		int r = Math.floorDiv(y,Math.floorDiv((frame.getHeight()*2/3),array.length));   // Position / height of the frame. Height is FrameHeight/No.of grids.
+		int c = Math.floorDiv(x,Math.floorDiv(frame.getWidth(),array[0].length));		// Position / width of the frame.
+		int[] a = new int[2];
+		a[0]=r;
+		a[1]=c;
+		
+		return a;
+	}
+	
+	
+	public static void main(String args[])
+	{
+		initialise();
+		solutions=0;
+		frame = new JFrame("Knight's tour");
+		Panel graphicsPanel = new Panel();
+		
+		
+		graphicsPanel.addMouseListener(new Mouse());
+		frame.setLayout(new GridLayout(1,1));
+		
+		//frame.add(textPanel);
+		frame.setSize(600, 700);
+		frame.add(graphicsPanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }
