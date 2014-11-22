9aa210ae30219ce56306507e3ff7bc86962a38cb
Laksha-Prashanth <lakshaprashanth@gmail.com>
2014-11-22 05:46:51 +0530

    FUCKING DONE!
    Finished the Knight's Tour. The algorithm works correctly for m and n =
    5.
    
    Now to work on the GUI.

--- src/KnightsTour.java
+++ src/KnightsTour.java
@@ -5,7 +5,9 @@ public class KnightsTour {
 	
 	public static int[][] array;
 	
-	public static void display()
+	public static int solutions;
+	
+	public static String display()
 	{
 		String s = "";
 		for(int i=0;i<array.length;i++)
@@ -24,6 +26,7 @@ public class KnightsTour {
 			s+='\n';
 		}
 		System.out.println(s);
+		return s;
 	}
 	
 	
@@ -31,7 +34,7 @@ public class KnightsTour {
 	public static boolean canMove(int r,int c,int row,int col)
 	{
 		
-		if(row<0||row>array.length||col<0||col>array[0].length)
+		if(row<0||row>=array.length||col<0||col>=array[0].length)
 			return false;
 		
 		if(Math.abs(row-r)>=3||Math.abs(col-c)>=3)
@@ -42,90 +45,126 @@ public class KnightsTour {
 		return true;
 	}
 	
-	public static List<String> validPlaces(int r,int c)
+	public static int[][] validPlaces(int r,int c)
 	{
 		List<String> list = new ArrayList<String>();
 		
 		String s = "";
 		
-		if(canMove(r,c,r-2,c+1)==true)
+		if(canMove(r,c,r-2,c+1)==true&&array[r-2][c+1]==0)
 		{
 			s=(r-2)+","+(c+1);
 			list.add(s);
 			//System.out.println((r-2)+","+(c+1));
 		}
 		
-		if(canMove(r,c,r-1,c+2)==true)
+		if(canMove(r,c,r-1,c+2)==true&&array[r-1][c+2]==0)
 		{
-			s=(r+1)+","+(c+2);
+			s=(r-1)+","+(c+2);
 			list.add(s);
 			//System.out.println((r-1)+","+(c+2));
 		}
-		if(canMove(r,c,r+1,c+2)==true)
+		if(canMove(r,c,r+1,c+2)==true&&array[r+1][c+2]==0)
 		{
 			s=(r+1)+","+(c+2);
 			list.add(s);
 			//System.out.println((r+1)+","+(c+2));
 		}
-		if(canMove(r,c,r+2,c+1)==true)
+		if(canMove(r,c,r+2,c+1)==true&&array[r+2][c+1]==0)
 		{
 			s=(r+2)+","+(c+1);
 			list.add(s);
 			//System.out.println((r+2)+","+(c+1));
 		}
-		if(canMove(r,c,r+2,c-1)==true)
+		if(canMove(r,c,r+2,c-1)==true&&array[r+2][c-1]==0)
 		{
 			s=(r+2)+","+(c-1);
 			list.add(s);
 			//System.out.println((r+2)+","+(c-1));
 		}
-		if(canMove(r,c,r+1,c-2)==true)
+		if(canMove(r,c,r+1,c-2)==true&&array[r+1][c-2]==0)
 		{
 			s=(r+1)+","+(c-2);
 			list.add(s);
 			//System.out.println((r+1)+","+(c-2));
 		}
-		if(canMove(r,c,r-1,c-2)==true)
+		if(canMove(r,c,r-1,c-2)==true&&array[r-1][c-2]==0)
 		{
 			s=(r-1)+","+(c-2);
 			list.add(s);
 			//System.out.println((r-1)+","+(c-2));
 		}
-		if(canMove(r,c,r-2,c-1)==true)
+		if(canMove(r,c,r-2,c-1)==true&&array[r-2][c-1]==0)
 		{
 			s=(r-2)+","+(c-1);
 			list.add(s);
 		}
-		System.out.println(list);
-		return list;
+		
+		int[][] a = new int[list.size()][2];
+		
+		for(int i=0;i<list.size();i++)
+		{
+			a[i][0]=Integer.parseInt(list.get(i).charAt(0)+"");
+			a[i][1]=Integer.parseInt(list.get(i).charAt(2)+"");
+		}
+		
+		
+		return a;
 	}
 	
-	public static boolean knight(int r,int c)
+	public static boolean checkOnes()
 	{
 		
+		for(int i=0;i<array.length;i++)
+		{
+			for(int j=0;j<array[0].length;j++)
+			{
+				if(array[i][j]==0)
+					return false;
+			}
+		}
 		
+		return true;
+	}
+	
+	
+	public static void knight(int r,int c)
+	{
+		int[][] a = validPlaces(r,c);
 		
-		return false;
+		for(int i=0;i<a.length;i++)
+		{
+			array[a[i][0]][a[i][1]] = 1;
+			//display();
+			if (checkOnes() == true)
+			{
+				solutions++;
+			}
+			knight(a[i][0], a[i][1]);
+		}
+		array[r][c]=0;
 	}
 	
 	public static void main(String args[])
 	{
+		solutions=0;
 		String s=JOptionPane.showInputDialog("Enter the size of the board");
 		
 		int n = Integer.parseInt(s.charAt(0)+"");
 		int m = Integer.parseInt(s.charAt(2)+"");
 		
 		array = new int[n][m];
-		
-		display();
-		
-		System.out.println(canMove(0,0,2,2));
-		List<String> b = validPlaces(2,2);
-		for(int i=0;i<b.size();i++)
+		for(int i=0;i<array.length;i++)
 		{
-			System.out.println(b.get(i));
-			//System.out.println(Integer.parseInt((b.get(i).charAt(0))+"")+","+Integer.parseInt((b.get(i).charAt(2))+""));
+			for(int j=0;j<array[0].length;j++)
+			{
+				array[i][j]=0;
+			}
 		}
+		
+		
+		System.out.println();
+		System.out.println();
 		
 	}
 }
